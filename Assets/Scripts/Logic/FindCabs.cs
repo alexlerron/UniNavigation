21793e5809bec2f4322ffbcf0288d92aebc7373a
Ivan <39524682+Skrals@users.noreply.github.com>
2021-05-24 04:27:04 +0300

    Найденный объект помечается

--- Assets/Scripts/Logic/FindCabs.cs
+++ Assets/Scripts/Logic/FindCabs.cs
@@ -7,6 +7,8 @@ public class FindCabs : MonoBehaviour
 {
     public GameObject[] cabsArray;
     public int desiredCab;
+    [SerializeField] private GameObject door;
+    [SerializeField] private GameObject tmp;
     void Start()
     {
         cabsArray = GameObject.FindGameObjectsWithTag("cab");
@@ -15,22 +17,40 @@ public class FindCabs : MonoBehaviour
     public void FindCab()
     {
         int i = 0;
+        DeleteDoor();
         foreach (GameObject go in cabsArray)
         {
             int cab = go.GetComponent<CabData>().cabNumber;
             if (cab == desiredCab)
             {
+                CreateDoor(go);
                 Log($"Кабинет номер {desiredCab} найден. {go.name}");
                 break;
             }
             else
             {
-                if(i == cabsArray.Length-1)
+                if (i == cabsArray.Length - 1)
                 {
                     Log($"Кабинет номер {desiredCab} не найден.");
                 }
             }
             i++;
+        }
+    }
+    void CreateDoor(GameObject go)
+    {
+        tmp = go;
+        Instantiate(door, tmp.transform);
+    }
+    void DeleteDoor()
+    {
+        if (tmp != null)
+        {
+            foreach (Transform child in tmp.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            tmp = null;
         }
     }
 }
