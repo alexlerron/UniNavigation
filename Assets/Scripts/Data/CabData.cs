bad3f1b6c6caf73cb7e1f0a1126c834584864c50
Ivan <39524682+Skrals@users.noreply.github.com>
2022-10-22 20:33:01 +0300

    Фильтр по типу кабинетов

--- Assets/Scripts/Data/CabData.cs
+++ Assets/Scripts/Data/CabData.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 
+public enum TypeName { Cabinet, Food, WC, Library, OfficeCab, Wardrobe }
+
 public class CabData : MonoBehaviour
 {
+    public TypeName typeName;
+    public bool isCabActive;
     public int cabNumber;
 }
